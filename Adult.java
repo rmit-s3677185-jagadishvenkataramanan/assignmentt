d65be0041f4002fe78360470b6901ac488851661
rmit-s3677185-jagadishvenkataramanan <31242090+rmit-s3677185-jagadishvenkataramanan@users.noreply.github.com>
2018-04-04 08:09:46 -0700

    adult
    //author jagadish

---
+++ Adult.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Adult extends User {
+	//author jagadish
+
+    private ArrayList<Child> childID;
+    public Adult(String name, String status, String image, int age)
+    {
+        this.name=name;
+        this.status=status;
+        this.image=image;
+        this.age=age;
+        this.friendList=new ArrayList<User>();
+        childID=new ArrayList<Child>();
+    }
+
+    public void connectChild(Child child)
+    {
+        childID.add(child);
+    }
+
+    public void displayChildren()
+    {
+        System.out.println("Following are the children of "+name);
+        for (Child child: childID) {
+            System.out.println(child.getName());
+        }
+    }
+
+
+
+}
